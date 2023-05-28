01f5cfc749c309a908bb6f3cec034d4c08f18fb4
Mario <mariokozul24@gmail.com>
2023-05-28 13:50:08 +0200

    added logic for changing grid size and coloring grid elements

--- source.js
+++ source.js
@@ -1,13 +1,30 @@
-let RowColumnNumber=parseInt(prompt('Enter number from 2 to 100'));
-const grid=document.querySelector('#container');
-grid.style.gridTemplateRows = 'repeat(' + RowColumnNumber + ', ' + (400 / RowColumnNumber) + 'px)';
-grid.style.gridTemplateColumns = 'repeat(' + RowColumnNumber + ', ' + (400 / RowColumnNumber) + 'px)';
-for(let i=0;i<RowColumnNumber*RowColumnNumber;i++){
+function start(RowColumnNumber){
+    const grid=document.querySelector('#container');
+    grid.innerHTML = '';
+    grid.style.gridTemplateRows = 'repeat(' + RowColumnNumber + ', ' + (400 / RowColumnNumber) + 'px)';
+    grid.style.gridTemplateColumns = 'repeat(' + RowColumnNumber + ', ' + (400 / RowColumnNumber) + 'px)';
 
-    const grid_el=document.createElement('div');
-    grid_el.classList.add('grid-item');
-    grid.appendChild(grid_el);
+    for(let i=0;i<RowColumnNumber*RowColumnNumber;i++){
+        const grid_el=document.createElement('div');
+        grid_el.classList.add('grid-item');
+        grid.appendChild(grid_el);
+    }
+    changeColor();
 }
+function changeRowColumnNumber(input){
+     start(input);
+}
+
+function changeColor(){
+    const gridEl=document.querySelectorAll('.grid-item');
+    gridEl.forEach(gridel=>{
+        gridel.addEventListener('mouseover',()=>{
+            gridel.style.backgroundColor='red';
+        })
+    });
+}
+
+start(16);
 
 
 
